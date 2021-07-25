4416ffd04f2ac8a8d65729bc32df2647a84c4f62
lasseb <lbenninga@schubergphilis.com>
2021-07-25 15:39:08 +0200

    Add parquet format reading

--- lambda_sns_google_sheets/lib/utils.py
+++ lambda_sns_google_sheets/lib/utils.py
@@ -16,11 +16,14 @@ def extract_messages(event: dict):
 
 def read_df_from_s3(bucket: str, key: str, format: str) -> pd.DataFrame:
     retr = s3.get_object(Bucket=bucket, Key=key)
+    body_as_bytes = BytesIO(retr['Body'].read())
 
     if format == 'json':
-        return pd.read_json(BytesIO(retr['Body'].read()))
+        return pd.read_json(body_as_bytes)
+    elif format == 'parquet':
+        return pd.read_parquet(body_as_bytes)
     elif format == 'csv':
-        return pd.read_csv(BytesIO(retr['Body'].read()))
+        return pd.read_csv(body_as_bytes)
         df = pd.read_csv(path)
     elif format == 'feather':
-        return pd.read_feather(BytesIO(retr['Body'].read()))
+        return pd.read_feather(body_as_bytes)
